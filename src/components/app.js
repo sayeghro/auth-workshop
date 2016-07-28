038777f279925f9ff123623b40fbdf70bcadfaa3
Robert Sayegh <robert.sayegh@dali.dartmouth.edu>
2016-07-28 14:52:55 -0400

    fb ath:

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 // example class based component (smart component)
 class App extends Component {
@@ -8,13 +9,29 @@ class App extends Component {
     this.state = {
 
     };
+    this.onFacebookLogin = this.onFacebookLogin.bind(this);
   }
 
+  onFacebookLogin(response) {
+    if (response.status !== 'not_authorized' && response.status !== 'unknown') {
+      document.getElementById('facebook').style = 'display: block';
+    } else {
+      document.getElementById('facebook').innerHTML = `Welcome, ${response.name}! `;
+      document.getElementById('facebook').innerHTML += `<img src="${response.picture.data.url}"/>`;
+      document.getElementById('facebook').style = 'display: block';
+    }
+  }
 
   render() {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
-      <div id="login">
+      <div id="facebook">
+        <FacebookLogin
+          appId="280170869017749"
+          autoLoad
+          fields="name,email,picture.type(large)"
+          callback={this.onFacebookLogin}
+        />
       </div>
     );
   }
